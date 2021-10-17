086a3e79084cf05233fc9025bdb2120f6c4f8035
gitevanhsu <aqwe78965@gmail.com>
2021-10-17 10:57:05 +0800

    Add fillter funtion

--- read.py
+++ read.py
@@ -12,3 +12,11 @@ for i in range(len(data)):
     total += len(data[i])
 print(total)
 print("留言平均長度為：", total / len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+
